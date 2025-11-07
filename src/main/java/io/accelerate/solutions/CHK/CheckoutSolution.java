31ba0cb0c55d8f7d58faf59c84f9261aa905e7a8
sbx_user1051 <sbx_user1051@169.254.22.199>
2025-11-07 06:50:01 +0000

    Fri Nov 07 06:50:01 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -18,6 +18,26 @@ public class CheckoutSolution {
         priceMap.put('D', 15);
         priceMap.put('E', 40);
         priceMap.put('F', 10);
+        priceMap.put('G', 20);
+        priceMap.put('H', 10);
+        priceMap.put('I', 35);
+        priceMap.put('J', 60);
+        priceMap.put('K', 80);
+        priceMap.put('L', 90);
+        priceMap.put('M', 15);
+        priceMap.put('N', 40);
+        priceMap.put('O', 10);
+        priceMap.put('P', 50);
+        priceMap.put('Q', 30);
+        priceMap.put('R', 50);
+        priceMap.put('S', 30);
+        priceMap.put('T', 20);
+        priceMap.put('U', 40);
+        priceMap.put('V', 50);
+        priceMap.put('W', 20);
+        priceMap.put('X', 90);
+        priceMap.put('Y', 10);
+        priceMap.put('Z', 50);
 
         Map<Character, Integer> count = new HashMap<>();
         for(char c: skus.toCharArray()) {
@@ -29,7 +49,6 @@ public class CheckoutSolution {
 
         int numE = count.getOrDefault('E', 0);
         int numB = count.getOrDefault('B', 0);
-
         int freeB = numE / 2;
         int payB = Math.max(0, numB - freeB);
         count.put('B', payB);
@@ -38,6 +57,22 @@ public class CheckoutSolution {
         int freeF = numF / 3;
         int payF = numF - freeF;
         count.put('F', payF);
+
+        int numN = count.getOrDefault('N', 0);
+        int numM = count.getOrDefault('M', 0);
+        int freeM = numN / 3;
+        count.put('M', Math.max(0, numM - freeM));
+
+        int numR = count.getOrDefault('R', 0);
+        int numQ = count.getOrDefault('Q', 0);
+        int freeQ = numR / 3;
+        count.put('Q', Math.max(0, numQ - freeQ));
+
+        int numU = count.getOrDefault('U', 0);
+        int freeU = numU / 4;
+        count.put('U', numU - freeU);
+
+
 
         int total = 0;
 
@@ -68,6 +103,74 @@ public class CheckoutSolution {
                 case 'F':
                     total += quantity * 10;
                     break;
+                case 'G':
+                    total += quantity * 20;
+                    break;
+                case 'H':
+                    total += (quantity / 10) * 80;
+                    quantity %= 10;
+                    total += (quantity / 5) * 45;
+                    quantity %= 5;
+                    total += quantity * 10;
+                    break;
+                case 'I':
+                    total += quantity * 35;
+                    break;
+                case 'J':
+                    total += quantity * 60;
+                    break;
+                case 'K':
+                    total += (quantity / 2) * 150 + (quantity % 2) * 80;
+                    break;
+                case 'L':
+                    total += quantity * 90;
+                    break;
+                case 'M':
+                    total += quantity * 15;
+                    break;
+                case 'N':
+                    total += quantity * 40;
+                    break;
+                case 'O':
+                    total += quantity * 10;
+                    break;
+                case 'P':
+                    total += (quantity / 5) * 200 + (quantity % 5) * 50;
+                    break;
+                case 'Q':
+                    total += (quantity / 3) * 80 + (quantity % 3) * 30;
+                    break;
+                case 'R':
+                    total += quantity * 50;
+                    break;
+                case 'S':
+                    total += quantity * 30;
+                    break;
+                case 'T':
+                    total += quantity * 20;
+                    break;
+                case 'U':
+                    total += quantity * 40;
+                    break;
+                case 'V':
+                    total += (quantity / 3) * 130;
+                    quantity %= 3;
+                    total += (quantity / 2) * 90;
+                    quantity %= 2;
+                    total += quantity * 50;
+                    break;
+                case 'W':
+                    total += quantity * 20;
+                    break;
+                case 'X':
+                    total += quantity * 90;
+                    break;
+                case 'Y':
+                    total += quantity * 10;
+                    break;
+                case 'Z':
+                    total += quantity * 50;
+                    break;
                 default:
                     return -1;
             }
@@ -78,3 +181,4 @@ public class CheckoutSolution {
         //throw new SolutionNotImplementedException();
     }
 }
+
