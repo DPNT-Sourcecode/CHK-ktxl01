7341006431a81e8ac736aa5511c77bd5613c3635
sbx_user1051 <sbx_user1051@169.254.22.199>
2025-11-07 06:10:23 +0000

    Fri Nov 07 06:10:23 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -16,6 +16,7 @@ public class CheckoutSolution {
         priceMap.put('B', 30);
         priceMap.put('C', 20);
         priceMap.put('D', 15);
+        priceMap.put('E', 40);
 
         Map<Character, Integer> count = new HashMap<>();
         for(char c: skus.toCharArray()) {
@@ -25,6 +26,13 @@ public class CheckoutSolution {
             count.put(c, count.getOrDefault(c, 0) + 1);
         }
 
+        int numE = count.getOrDefault('E', 0);
+        int numB = count.getOrDefault('B', 0);
+
+        int freeB = numE / 2;
+        int payB = Math.max(0, numB - freeB);
+        count.put('B', payB);
+
         int total = 0;
 
         for(Map.Entry<Character, Integer> entry: count.entrySet()) {
@@ -33,7 +41,11 @@ public class CheckoutSolution {
 
             switch (item) {
                 case 'A':
-                    total += (quantity / 3) * 130 + (quantity % 3) * 50;
+                    total += (quantity / 5) * 200;
+                    quantity %= 5;
+                    total += (quantity / 3) * 130;
+                    quantity %= 3;
+                    total += quantity * 50;
                     break;
                 case 'B':
                     total += (quantity / 2) * 45 + (quantity % 2) * 30;
@@ -44,6 +56,11 @@ public class CheckoutSolution {
                 case 'D':
                     total += quantity * 15;
                     break;
+                case 'E':
+                    total += quantity * 40;
+                    break;
+                default:
+                    return -1;
             }
         }
 
@@ -52,3 +69,4 @@ public class CheckoutSolution {
         //throw new SolutionNotImplementedException();
     }
 }
+
